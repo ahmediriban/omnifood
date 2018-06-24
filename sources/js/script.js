3195737389b16515e2968c87d239dc86da879965
ahmediriban <ahmedireban@gmail.com>
2018-06-24 16:53:52 +0300

    Add files via upload

---
+++ sources/js/script.js
@@ -0,0 +1,123 @@
+
+$(document).ready(function () {
+
+    $('.js--feature--section').waypoint(function (direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+            offset: '100px'
+    });
+
+
+
+
+
+    $('.js--scroll--to--plans').click(function () {
+        $('html, body').animate({scrollTop:$('.js--plan--section').offset().top}, 1000)
+    })
+
+    $('.js--scroll--to--feature').click(function () {
+        $('html, body').animate({scrollTop:$('.js--feature--section').offset().top}, 1000)
+    })
+
+
+
+
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+
+
+    $('.js--wp--1').waypoint(function (direction) {
+        $('.js--wp--1').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    }
+    )
+
+    $('.js--wp--4').waypoint(function (direction) {
+            $('.js--wp--4').addClass('animated fadeIn');
+        },{
+            offset: '50%'
+        }
+    )
+
+    $('.js--wp--2').waypoint(function (direction) {
+            $('.js--wp--2').addClass('animated fadeInUp');
+        },{
+            offset: '50%'
+        }
+    )
+
+    $('.js--wp--3').waypoint(function (direction) {
+            $('.js--wp--3').addClass('animated pulse');
+        },{
+            offset: '60%'
+        }
+    )
+
+
+    $('.mobile--nav').click(function () {
+        var nav = $('.main-nav');
+        var icon = $('.mobile-icon');
+
+        nav.slideToggle(200);
+
+        if(icon.hasClass('ion-md-menu')){
+            icon.addClass('ion-md-close');
+            icon.removeClass('ion-md-menu');
+        }else{
+            icon.addClass('ion-md-menu');
+            icon.removeClass('ion-md-close');
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+});
